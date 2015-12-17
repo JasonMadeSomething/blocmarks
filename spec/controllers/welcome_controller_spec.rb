8723355bb2613d681c3ba709f0c0f340e351e3fb
Jason <jason@mailmarketingfla.com>
2015-12-17 17:46:48 +0000

    Write initial Welcome Controller tests

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #home" do
+    before { get :home}
+    
     it "returns http success" do
-      get :home
       expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the home template" do
+      expect(response).to render_template("home")
     end
   end
 
