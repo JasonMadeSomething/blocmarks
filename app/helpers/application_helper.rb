a0cf4a1d8506094ae1ee4478bf4bf5a9e13f3225
Jason <jason@mailmarketingfla.com>
2015-12-18 20:23:15 +0000

    Start adding embedly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+    require 'embedly'
+    require 'json'
+    
+    def display_title(url)
+       embedly_api = Embedly::API.new(key: ENV['embedly_key'])
+       obj = embedly_api.oembed url: url
+       (obj.first.title).html_safe
+    end
+    def display_thumbnail(url)
+       embedly_api = Embedly::API.new(key: ENV['embedly_key'])
+       obj = embedly_api.oembed url: url
+       (obj.first.thumbnail_url).html_safe
+    end
+    def display_description(url)
+       embedly_api = Embedly::API.new(key: ENV['embedly_key'])
+       obj = embedly_api.oembed url: url
+       (obj.first.description).html_safe
+    end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,5 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :topic
   
-  self.per_page = 3
   
 end
