174cd472daeebaa89ba716e1ef07f30402408a66
Jason <jason@mailmarketingfla.com>
2016-01-12 18:23:04 +0000

    Create topics and bookmarks via email

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,7 +3,14 @@ class IncomingController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:create]
     
     def create
-        
+        user = User.find_by(email: params[:sender])
+        if user
+            topic = Topic.find_by(title: params[:subject])
+            topic = Topic.create(title: params[:subject], user: user) unless topic
+            url = params["body-plain"]
+            Bookmark.create(url: url, topic: topic)
+        end
+        head 200
     end
     
 end
